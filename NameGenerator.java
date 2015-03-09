9a780cb4da4c47e58e74d991bf102474345631c7
Julia Zuara <julia_any@hotmail.com>
2015-03-09 09:24:22 +0100

    Primera versión. No tiene en cuenta aun casos que no cumplan con la longitud minima

---
+++ NameGenerator.java
@@ -0,0 +1,44 @@
+
+/**
+ * Genera nombres de tipo StarWars, de la siguiente forma:
+ * - Tu nombre seria:
+ *  - Toma las tres primeras letras de tu apellido real
+ *  - Toma las tres primeras letras de tu nombre real
+ * - Tu apellido seria:
+ *  - Toma las tres primeras letras del apellido real de tu madre
+ *  - Añade las tres primeras letras de la ciudad real donde nacistes
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NameGenerator
+{
+    // Guarda el numero de posiciones que tenemos que tomar de cada palabra para formar el nombre
+    private static final int POSICIONES = 3;
+   
+
+    /**
+     * Constructor for objects of class NameGenerator
+     */
+    public NameGenerator()
+    {
+       
+    }
+
+    /**
+     *Genera nombres tipo StarWars
+     *@param Apellido Tu apellido
+     *@param Nombre Tu nombre
+     *@param ApellidoMaterno Tu segundo apellido
+     *@param Ciudad Ciudad de nacimiento
+     */
+    public String sampleMethod(String nombre, String apellido, String apellidoMaterno, String ciudad)
+    {
+        String resultado = "";
+        // Generamos el nombre
+        resultado = nombre.substring(0, POSICIONES) + apellido.toLowerCase().substring(0, POSICIONES);
+        // Generamos el apellido
+        resultado = resultado + " " + apellidoMaterno.substring(0, POSICIONES) + ciudad.toLowerCase().substring(0, POSICIONES);
+        return resultado;
+    }
+}
